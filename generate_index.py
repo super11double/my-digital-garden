17d94a589a026084c1842dfcf740e2883257d771
super11double <2671727956@qq.com>
2026-08-08 20:24:05 +0800

    Update generate_index.py

--- generate_index.py
+++ generate_index.py
@@ -1,25 +1,27 @@
 import os
 import glob
-import re
+import urllib.parse
 
-def has_chinese(text):
-    return re.search('[\u4e00-\u9fff]', text) is not None
+IGNORE_BASENAMES = {'generate_index.py', 'README.md'}
+IGNORE_DIRS = {'.git', 'node_modules', '.venv', 'venv', '.obsidian'}
 
-md_files = glob.glob('**/*.md', recursive=True)
 md_files = [
-    f for f in md_files
-    if f not in ['generate_index.py', 'README.md']
-    and not has_chinese(os.path.basename(f))
+    f for f in glob.glob('**/*.md', recursive=True)
+    if os.path.basename(f) not in IGNORE_BASENAMES
+    and not any(part in IGNORE_DIRS for part in f.split(os.sep))
 ]
 
 list_items = ""
 for file in sorted(md_files):
-    name = os.path.splitext(file)[0].replace(os.sep, '/')
-    list_items += f'        <li><a href="{name}">{name}</a></li>\n'
+    path_no_ext = os.path.splitext(file)[0].replace(os.sep, '/')
+    href = urllib.parse.quote(path_no_ext + '.md', safe='/')
+    display = os.path.splitext(os.path.basename(file))[0]
+    list_items += f'        <li><a href="{href}">{display}</a></li>\\n'
 
 html_content = f'''<!DOCTYPE html>
 <html>
 <head>
+    <meta charset="utf-8" />
     <title>我的知识库</title>
 </head>
 <body>
